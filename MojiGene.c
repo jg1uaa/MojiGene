411ad43cede3d87755da651d28b51eb224836751
SASANO Takayoshi <uaa@uaa.org.uk>
2025-05-28 21:09:47 +0900

    add CharGroup0/CharGroup1 support

--- MojiGene.c
+++ MojiGene.c
@@ -22,13 +22,18 @@ static int Chars = 300;
 static int NumRatio = 0x20;
 static int SleepTime = 0;
 static int WordPerLine = 5;
+static int CharGroup0Len;
+static int CharGroup1Len;
 static char Header[BUFSIZE] = "hr hr <bt>";
 static char Footer[BUFSIZE] = "<ar>";
 static char FileName[BUFSIZE] = "MojiGene.txt";
+static char CharGroup0[BUFSIZE] = "abcdefghijklmnopqrstuvwxyz";
+static char CharGroup1[BUFSIZE] = "0123456789";
 
 struct config {
 	char *string;
 	int (*function)(char *);
+	bool allow_empty;
 };
 
 static int set_wordlen(char *);
@@ -40,18 +45,22 @@ static int set_wordperline(char *);
 static int set_header(char *);
 static int set_footer(char *);
 static int set_filename(char *);
+static int set_chargroup0(char *);
+static int set_chargroup1(char *);
 
 static struct config keywords[] = {
 	/* need a space after keyword */
-	{"WordLen ", set_wordlen},
-	{"Chars ", set_chars},
-	{"Words ", set_charsbywords},
-	{"NumRatio ", set_numratio},
-	{"SleepTime ", set_sleeptime},
-	{"WordPerLine ", set_wordperline},
-	{"Header ", set_header},
-	{"Footer ", set_footer},
-	{"FileName ", set_filename},
+	{"WordLen ", set_wordlen, false},
+	{"Chars ", set_chars, false},
+	{"Words ", set_charsbywords, false},
+	{"NumRatio ", set_numratio, false},
+	{"SleepTime ", set_sleeptime, false},
+	{"WordPerLine ", set_wordperline, false},
+	{"Header ", set_header, true},
+	{"Footer ", set_footer, true},
+	{"FileName ", set_filename, false},
+	{"CharGroup0 ", set_chargroup0, true},
+	{"CharGroup1 ", set_chargroup1, true},
 };
 
 static int set_wordlen(char *buf)
@@ -108,6 +117,29 @@ static int set_filename(char *buf)
 	return 0;
 }
 
+static void remove_space_copy(char *dst, int dstsize, char *src)
+{
+	int i;
+
+	for (i = 0; *src && i < dstsize - 1; src++)
+		if (*src != ' ') dst[i++] = *src;
+
+	dst[i] = '\0';
+}
+
+static int set_chargroup0(char *buf)
+{
+	remove_space_copy(CharGroup0, sizeof(CharGroup0), buf);
+	CharGroup1[0] = '\0';
+	return 0;
+}
+
+static int set_chargroup1(char *buf)
+{
+	remove_space_copy(CharGroup1, sizeof(CharGroup1), buf);
+	return 0;
+}
+
 static char *skip_spaces(char *buf)
 {
 	char *p;
@@ -115,6 +147,13 @@ static char *skip_spaces(char *buf)
 	for (p = buf; *p == ' '; p++);
 
 	return p;
+}
+
+static void remove_trailing_spaces(char *buf)
+{
+	char *p;
+
+	for (p = buf + strlen(buf) - 1; p >= buf && *p == ' '; *p-- = '\0');
 }
 
 static int parse(char *buf)
@@ -126,7 +165,9 @@ static int parse(char *buf)
 	if ((p = strchr(buf, '\x0a')) != NULL) *p = '\0';
 	if ((p = strchr(buf, '\x0d')) != NULL) *p = '\0';
 
-	p = skip_spaces(buf);
+	/* '#' as comment line */
+	if (*(p = skip_spaces(buf)) == '#')
+		return 0;
 
 	/* '=' separator required */
 	if ((q = strchr(p, '=')) == NULL)
@@ -134,15 +175,12 @@ static int parse(char *buf)
 
 	*q = '\0';
 	q = skip_spaces(q + 1);
+	remove_trailing_spaces(q);
 	
-	/* value required */
-	if (*q == '\0')
-		return -1;
-
 	for (i = 0; i < sizeof(keywords) / sizeof(struct config); i++) {
 		if (!strcmp(p, keywords[i].string))
-			return (*keywords[i].function)(q);
-
+			return (keywords[i].allow_empty || *q) ?
+				(*keywords[i].function)(q) : -1;
 	}
 
 	return -1;
@@ -157,10 +195,8 @@ static int do_config(void)
 	if (fp == NULL)
 		return -1;
 
-	while (fgets(buf, sizeof(buf), fp) != NULL) {
-		if (*buf == '#') continue;
+	while (fgets(buf, sizeof(buf), fp) != NULL)
 		parse(buf);
-	}
 
 	fclose(fp);
 
@@ -169,10 +205,10 @@ static int do_config(void)
 
 static int mojigene_ch(void)
 {
-	if ((rand() & 0x7f) < NumRatio)
-		return '0' + (rand() % 10);
+	if (CharGroup1Len > 0 && (rand() & 0x7f) < NumRatio)
+		return CharGroup1[rand() % CharGroup1Len];
 	else
-		return 'a' + (rand() % 26);
+		return CharGroup0[rand() % CharGroup0Len];
 }
 			
 static void mojigene(FILE *fp)
@@ -201,7 +237,8 @@ static int do_main(void)
 		return -1;
 
 	if (strlen(Header)) fprintf(fp, "%s" CRLF, Header);
-	if (WordLen > 0 && WordPerLine > 0 && Chars > 0) mojigene(fp);
+	if (WordLen > 0 && WordPerLine > 0 &&
+	    Chars > 0 && CharGroup0Len > 0) mojigene(fp);
 	if (strlen(Footer)) fprintf(fp, "%s" CRLF, Footer);
 
 	fclose(fp);
@@ -213,6 +250,7 @@ int main(int argc, char *argv[])
 {
 	int ch;
 	time_t t;
+	char *p;
 	bool debug = false;
 
 	t = time(NULL);
@@ -221,20 +259,30 @@ int main(int argc, char *argv[])
 	do_config();
 
 	/* override by command line */
-	while ((ch = getopt(argc, argv, "W:c:w:n:s:L:H:F:o:d")) != -1) {
+	while ((ch = getopt(argc, argv, "W:c:w:n:s:L:H:F:o:x:y:d")) != -1) {
+		if ((p = optarg) != NULL) {
+			p = skip_spaces(optarg);
+			remove_trailing_spaces(p);
+		}
+
 		switch (ch) {
-		case 'W': set_wordlen(optarg); break;
-		case 'c': set_chars(optarg); break;
-		case 'w': set_charsbywords(optarg); break;
-		case 'n': set_numratio(optarg); break;
-		case 's': set_sleeptime(optarg); break;
-		case 'L': set_wordperline(optarg); break;
-		case 'H': set_header(optarg); break;
-		case 'F': set_footer(optarg); break;
-		case 'o': set_filename(optarg); break;
+		case 'W': set_wordlen(p); break;
+		case 'c': set_chars(p); break;
+		case 'w': set_charsbywords(p); break;
+		case 'n': set_numratio(p); break;
+		case 's': set_sleeptime(p); break;
+		case 'L': set_wordperline(p); break;
+		case 'H': set_header(p); break;
+		case 'F': set_footer(p); break;
+		case 'o': set_filename(p); break;
+		case 'x': set_chargroup0(p); break;
+		case 'y': set_chargroup1(p); break;
 		case 'd': debug = true; break;
 		}
 	}
+
+	CharGroup0Len = strlen(CharGroup0);
+	CharGroup1Len = strlen(CharGroup1);
 
 	if (debug) {
 		fprintf(stderr, "WordLen = %d\n", WordLen);
@@ -245,6 +293,10 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Header = \"%s\"\n", Header);
 		fprintf(stderr, "Footer = \"%s\"\n", Footer);
 		fprintf(stderr, "FileName = \"%s\"\n", FileName);
+		fprintf(stderr, "CharGroup0Len = %d\n", CharGroup0Len);
+		fprintf(stderr, "CharGroup1Len = %d\n", CharGroup1Len);
+		fprintf(stderr, "CharGroup0 = \"%s\"\n", CharGroup0);
+		fprintf(stderr, "CharGroup1 = \"%s\"\n", CharGroup1);
 	}
 
 #if defined(__LCC__)
